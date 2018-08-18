0079a28ac148757b13343039a771adef13998416
Paul Burz <burzpaull@gmail.com>
2018-08-18 18:34:49 +0300

    add html structure and models for recipe and ingredient

--- shopify/src/app/shopping-list/shopping-list.component.ts
+++ shopify/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/models/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 10),
+    new Ingredient('Potatos', 4),
+    new Ingredient('Cucumber', 3),
+    new Ingredient('Eggs', 12),
+  ];
 
   constructor() { }
 
