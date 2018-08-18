12161caa7f7f484deed7a442f6ea2665e13ea6ce
Paul Burz <burzpaull@gmail.com>
2018-08-18 16:37:16 +0300

    add recipe-list

--- shopify/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopify/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../models/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+  ];
 
   constructor() { }
 
