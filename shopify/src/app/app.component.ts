f3dce46a4777e449eb6857dd714c68a0c6366713
Paul Burz <burzpaull@gmail.com>
2018-08-19 14:32:56 +0300

    add basic navigation

--- shopify/src/app/app.component.ts
+++ shopify/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopify';
+  loadedPage = 'recipe';
+
+  onNavigate(page: string) {
+    this.loadedPage = page;
+  }
 }
