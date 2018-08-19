ab70dabafc78f0eade9490fd7329f789630b4f65
Paul Burz <burzpaull@gmail.com>
2018-08-19 15:01:07 +0300

    add recipe-detail

--- shopify/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ shopify/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from './../../models/recipe.model';
 
@@ -9,10 +9,13 @@ import { Recipe } from './../../models/recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected() {
+    this.recipeSelected.emit();
+  }
 }
--- shopify/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopify/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../models/recipe.model';
 
@@ -8,21 +8,24 @@ import { Recipe } from '../models/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 2', 'Recipe 2 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 3', 'Recipe 3 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 4', 'Recipe 4 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 5', 'Recipe 5 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 6', 'Recipe 6 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 7', 'Recipe 7 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 8', 'Recipe 8 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 9', 'Recipe 9 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipe.emit(recipe);
+  }
 }
