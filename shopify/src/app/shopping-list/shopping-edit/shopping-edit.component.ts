6732bee9d9dca9054f003b7131b6327db9d76e05
Paul Burz <burzpaull@gmail.com>
2018-08-19 16:09:48 +0300

    add shopping-edit

--- shopify/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shopify/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+
+import { Ingredient } from './../../shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+    const ingredient = new Ingredient(name, amount);
+    this.ingredientAdded.emit(ingredient);
+  }
 }
