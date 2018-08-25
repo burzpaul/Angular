0eca5d7a5ef4d2a4a99abb55b2477a9ea92990c3
Paul Burz <burzpaull@gmail.com>
2018-08-25 20:38:17 +0300

    add to shopping list logic

--- shopify/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ shopify/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../models/recipe.model';
 
+import { RecipeService } from './../services/recipe.service';
+
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -10,9 +12,12 @@ import { Recipe } from '../models/recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    this.recipeService.addIngredientToShoppingList(this.recipe.ingredients);
+  }
 }
--- shopify/src/app/recipes/services/recipe.service.ts
+++ shopify/src/app/recipes/services/recipe.service.ts
@@ -1,20 +1,27 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from '../models/recipe.model';
+import { Ingredient } from '../../shared/models/ingredient.model';
 
+import { ShoppingListService } from './../../shopping-list/services/shopping-list.service';
+
+@Injectable()
 export class RecipeService {
   private recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 2', 'Recipe 2 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 3', 'Recipe 3 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 4', 'Recipe 4 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 5', 'Recipe 5 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 6', 'Recipe 6 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 7', 'Recipe 7 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 8', 'Recipe 8 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
-    new Recipe('Recipe 9', 'Recipe 9 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg'),
+    new Recipe('Recipe 1', 'Recipe 1 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French fries', 21)
+      ]),
+    new Recipe('Recipe 2', 'Recipe 2 description', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ];
+
+  constructor(private shoppingListService: ShoppingListService) { }
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
@@ -22,5 +29,9 @@ export class RecipeService {
 
   getRecipeSelected(): EventEmitter<Recipe> {
     return this.recipeSelected;
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
