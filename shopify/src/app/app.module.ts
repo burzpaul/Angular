ad6b458723c7598f8d428e15a3a03717ea6dee33
Paul Burz <burzpaull@gmail.com>
2018-12-24 14:19:25 +0200

    refactor app module

--- shopify/src/app/app.module.ts
+++ shopify/src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 
+//#region Modules
+import { AppRoutingModule } from './app-routing.module';
+//#endregion
+
+//#region Component
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeInfoComponent } from './recipes/recipe-info/recipe-info.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+//#endregion
+
+//#region Services
+//#endregion
 
 import { DropdownDirective } from './shared/directives/dropdown.directive';
 
