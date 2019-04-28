0ceb913f2dc120c35c5e261e606e39bc9113ef04
Paul Burz <burzpaull@gmail.com>
2019-04-28 12:34:55 +0300

    add recipe reducer

---
+++ shopify/src/app/recipes/store/recipe.reducers.ts
@@ -0,0 +1,15 @@
+export interface FeatureState {
+  recipes: State;
+}
+
+export interface State {
+  recipes: Recipe[];
+}
+
+const initialState: State = {
+  recipes: [],
+};
+
+export function recipeReducer(state = initialState, action) {
+  return state;
+}
