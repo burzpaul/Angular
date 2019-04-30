05e50050344327613056626f0be5e379adaef6e0
Paul Burz <burzpaull@gmail.com>
2019-04-30 16:01:58 +0300

    add recipes fetch on init in recipe page

--- shopify/src/app/recipes/pages/recipes/recipes.page.ts
+++ shopify/src/app/recipes/pages/recipes/recipes.page.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '@app/store/app.reducers';
+import * as RecipeActions from '@recipes/store/recipe.actions';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss']
 })
-export class RecipesPageComponent {}
+export class RecipesPageComponent implements OnInit {
+  constructor(private store: Store<fromApp.AppState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(new RecipeActions.FetchRecipes());
+  }
+}
